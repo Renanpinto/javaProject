937f579325441370ed1ceee8199680a17d67fd3c
Renan Pinto <renanspinto@hotmail.com>
2019-05-21 14:31:50 -0300

    refactoring

--- src/main/java/project/q1/Test.java
+++ src/main/java/project/q1/Test.java
@@ -4,15 +4,13 @@ public class Test implements Runnable {
 
 	public void run() {
 		System.out.println("Qual é a musica?");
-		//throw new RuntimeException("Opa, temos um problema!");
+		throw new RuntimeException("Opa, temos um problema!");
 	}
 
 	public static void main(String[] args) {
-
 		Thread t = new Thread(new Test());
 		t.start();
 	
 		System.out.println("Executou o Final do Método.");
 	}
-
 }
--- src/main/java/project/q4/Test.java
+++ src/main/java/project/q4/Test.java
@@ -1,8 +1,7 @@
 package project.q4;
 
 public class Test {
-	public static void main(String[] args) { 
-	
-	System.out.println("hi");
+	public static void main(String[] args) {
+		System.out.println("hi");
 	}
 }
